4b31b354aa665208eb5c300f97c596e89ca00408
Nils Mueller <nils@platogo.com>
2017-06-14 22:32:43 +0200

    Load config at application start

--- lib/geo.ex
+++ lib/geo.ex
@@ -4,12 +4,22 @@ defmodule GEO do
   def start(_type, _args) do
     import Supervisor.Spec
 
+    configure_ip_database()
+
     children = [
       worker(GEO.RefreshDatabase, [])
     ]
 
     options = [strategy: :one_for_one, name: GEO.Supervisor]
     Supervisor.start_link(children, options)
+  end
+
+  defp configure_ip_database() do
+     Geolix.load_database(%{
+       id: :city,
+       adapter: Geolix.Adapter.MMDB2,
+       source: Const.encode(:ip_database_file)
+     })
   end
 
   # Refresh database file during compilation
